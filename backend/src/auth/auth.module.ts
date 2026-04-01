9fced2c1ccd99d37bf8546aa14b5d0ed051170d1
Pannnomaly <supawith007@gmail.com>
2026-04-01 23:13:16 +0700

    feat(auth): implement register/login with JWT, bcrypt and route guards

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy/jwt.strategy';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        secret: ConfigService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
+})
+export class AuthModule {}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async createUser(data: { email: string; password: string }) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+}
