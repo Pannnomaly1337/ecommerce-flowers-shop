6f03c6826b808d6f0fc1f8f10fb49fe18a04de04
Pannnomaly <supawith007@gmail.com>
2026-04-23 20:03:44 +0700

    feat(dashboard): implement dashboard logic and controller

--- backend/src/dashboard/dashboard.controller.ts
+++ backend/src/dashboard/dashboard.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { DashboardService } from 'src/dashboard/dashboard.service';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.ADMIN)
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(private dashboardService: DashboardService) {}
+
+  @Get()
+  getDashboard() {
+    return this.dashboardService.getDashboard();
+  }
+}
