0a973cae855db622daf76484191a0f7e32d47fb1
Pannnomaly <supawith007@gmail.com>
2026-04-23 17:29:54 +0700

    feat(dashboard): create dashboard module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
 import { OrdersModule } from './orders/orders.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { OrdersModule } from './orders/orders.module';
     ProductsModule,
     CartModule,
     OrdersModule,
+    DashboardModule,
   ],
   controllers: [AppController],
   providers: [AppService],
